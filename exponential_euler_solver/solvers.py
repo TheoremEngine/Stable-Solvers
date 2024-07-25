304af695b145e972bc22b2013bf339e00d3de8fd
Mark Lowell <marklowell@Marks-MacBook-Air.local>
2024-07-24 20:09:28 -0400

    Added warmup option to solvers

--- exponential_euler_solver/solvers.py
+++ exponential_euler_solver/solvers.py
@@ -78,20 +78,38 @@ class AdaptiveGradientDescentReport(SolverReport):
 class AdaptiveGradientDescent(Solver):
     '''
     Performs gradient descent without momentum, adapting the learning rate at
-    every step to prevent entering the edge of stability.
+    every step to prevent entering the edge of stability:
+
+    ..math::
+        \\theta_{u+1} = \\theta_u -
+        \\eta_u \\nabla_\\theta \\widetilde{\\mathcal{L}}(\\theta)
+
+        \\eta_u = \\min\\left(\\eta_{\\max}, \\frac{1}
+        {\\lambda^1(\\mathcal{H}_\\theta \\widetilde{\\mathcal{L}}(\\theta_u))}
+        \\right)
     '''
     def __init__(self, params: torch.Tensor, loss: LossFunction,
-                 lr: float):
+                 lr: float, warmup_iters: int = 0, warmup_factor: float = 1.):
         '''
         Args:
             params (:class:`torch.Tensor`): The parameters of the network that
                 are being optimized.
             loss (:class:`LossFunction`): The loss function.
-            lr (float): Learning rate.
+            lr (float): Maximum learning rate. If the adaptive learning rate
+                exceeds this value, it is truncated to be no higher than this.
+            warmup_iters (int): If set, the maximum learning rate is initially
+                set to a lower value for this many iterations, to damp out
+                initial transients.
+            warmup_factor (float): If set, the maximum learning rate is
+                initially reduced by this factor, to damp out initial
+                transients.
         '''
         super().__init__(params=params, loss=loss)
         self.lr = lr
+        self.warmup_iters = warmup_iters
+        self.warmup_factor = warmup_factor
         self.eigvec = None
+        self.i = 0
 
     def step(self) -> AdaptiveGradientDescentReport:
         # Calculate the gradient
@@ -101,9 +119,11 @@ class AdaptiveGradientDescent(Solver):
             self.loss, self.params, 1,
             init_eigvecs=self.eigvec, max_iters=1000,
         )
-        step_size = min(self.lr, 1. / sharpness.item())
+        scale = self.warmup_factor if (self.i < self.warmup_iters) else 1.
+        step_size = min(scale * self.lr, 1. / sharpness.item())
         # Perform update
         self.params -= step_size * grads
+        self.i += 1
         return AdaptiveGradientDescentReport(
             loss=loss.item(), dt=step_size, sharpness=sharpness.item(),
         )
@@ -116,7 +136,8 @@ class ExponentialEulerSolverReport(SolverReport):
 
 class ExponentialEulerSolver(Solver):
     def __init__(self, params: torch.Tensor, loss: LossFunction,
-                 max_step_size: float, stiff_dim: int):
+                 max_step_size: float, stiff_dim: int, warmup_iters: int = 0,
+                 warmup_factor: float = 1.):
         '''
         Args:
             params (:class:`torch.Tensor`): The parameters of the network that
@@ -126,11 +147,20 @@ class ExponentialEulerSolver(Solver):
             stiff_dim (int): Dimension of the expected "stiff" component
                 of the loss landscape, generally equal to the number of
                 network outputs.
+            warmup_iters (int): If set, the maximum step size is initially
+                set to a lower value for this many iterations, to damp out
+                initial transients.
+            warmup_factor (float): If set, the maximum step size is
+                initially reduced by this factor, to damp out initial
+                transients.
         '''
         super().__init__(params=params, loss=loss)
         self.max_step_size = max_step_size
         self.stiff_dim = stiff_dim
+        self.warmup_iters = warmup_iters
+        self.warmup_factor = warmup_factor
         self.eigvecs = None
+        self.i = 0
 
     def step(self) -> ExponentialEulerSolverReport:
         # Calculate the gradient
@@ -147,13 +177,17 @@ class ExponentialEulerSolver(Solver):
         stiff_projections = grads @ self.eigvecs[:, :-1]
         bulk = grads - self.eigvecs[:, :-1] @ stiff_projections
         # Calculate bulk component of the step
-        step_size = min(self.max_step_size, 1. / eigvals[-1].abs().item())
+        scale = self.warmup_factor if (self.i < self.warmup_iters) else 1.
+        step_size = min(
+            scale * self.max_step_size, 1. / eigvals[-1].abs().item()
+        )
         step = bulk * -step_size
         # Calculate stiff component of the step
         stiff_step = (1 - (-eigvals[:-1] * step_size).exp()) / eigvals[:-1]
         stiff_step = stiff_step.clamp_(max=step_size)
         step -= self.eigvecs[:, :-1] @ (stiff_step * stiff_projections)
         self.params += step
+        self.i += 1
 
         return ExponentialEulerSolverReport(
             loss=loss.item(), eigvals=eigvals, dt=step_size,
