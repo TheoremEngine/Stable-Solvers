8832dcac163043e25708706e1dbdf808458e95a8
Mark Lowell <marklowell@Marks-MacBook-Air.local>
2024-07-29 21:10:22 -0400

    Add logo

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+## Stable Solvers <img align="right" src="docs/source/_static/logo.png">
+
 This library provides solvers for training neural networks without entering the edge of stability. The edge of stability phenomenon was discovered in [Cohen et al. 2021](https://arxiv.org/abs/2103.00065). This repo is based on the approach used in [Lowell and Kastner 2024](https://arxiv.org/abs/2406.00127), but it is a complete reimplementation incorporating new techniques that improve runtime efficiency and support more recent versions of PyTorch. **IT IS STILL UNDER HEAVY CONSTRUCTION**.
 
 Training neural networks without entering the edge of stability requires departing somewhat from the conventional syntax in PyTorch, because the solver needs to have direct access to the loss function so it can calculate eigenvectors and eigenvalues. The recommended syntax in this repo is:
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,6 +3,101 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+import tempfile
+
+import matplotlib
+import matplotlib.pyplot as plot
+import numpy as np
+from PIL import Image
+import torch
+
+import stable_solvers as solvers
+
+N = 128
+
+
+class PseudoNet(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        pts = torch.zeros((2,))
+        self.register_parameter('pts', torch.nn.Parameter(pts))
+
+    def forward(self, x = None):
+        x, y = self.pts
+        return (((6 + 2 * y) * x ** 2) + y).view(1)
+
+
+def make_surface(path: str):
+    net = PseudoNet()
+
+    xs, ys = torch.meshgrid(
+        (torch.linspace(-1, 1, N), torch.linspace(0, 1, N)),
+        indexing='ij'
+    )
+    xys = torch.stack((xs, ys), dim=2)
+    zs = [
+        torch.func.functional_call(net, {'pts': xy}, ()).view(1)
+        for xy in xys.view(-1, 2)
+    ]
+    zs = torch.cat(zs).view(N, N)
+
+    fig, ax = plot.subplots(subplot_kw={'projection': '3d'}, figsize=(6, 6))
+    ax.plot_surface(xs, ys, zs, vmin=zs.min() * 2, cmap=matplotlib.cm.viridis)
+    ax.set_xlim(-1, 1)
+    ax.set_ylim(0, 1)
+    ax.set_zlim(0, 9)
+    plot.axis('off')
+    plot.savefig(path)
+    plot.close()
+
+
+def make_curve(path: str):
+    net = PseudoNet()
+
+    xy = torch.tensor([-0.9, 0.9])
+    xys, zs = [xy.clone()], []
+
+    loss_func = solvers.LossFunction(
+        torch.utils.data.TensorDataset(torch.zeros(1), torch.zeros(1)),
+        lambda x, y: x.view(1,),
+        net,
+    )
+    solver = solvers.ExponentialEulerSolver(
+        xy, loss_func, 0.001, 0,
+    )
+
+    while xys[-1][1] > 0:
+        zs.append(solver.step().loss)
+        xys.append(solver.params.clone())
+
+    xys = torch.stack(xys[:-1], dim=0)
+    zs = torch.tensor(zs)
+
+    fig, ax = plot.subplots(subplot_kw={'projection': '3d'}, figsize=(6, 6))
+    ax.plot(xys[:, 0], xys[:, 1], zs, color='yellow')
+    ax.set_xlim(-1, 1)
+    ax.set_ylim(0, 1)
+    ax.set_zlim(0, 9)
+    plot.axis('off')
+    plot.savefig(path)
+    plot.close()
+
+
+def make_logo(path: str):
+    with tempfile.TemporaryDirectory() as temp_root:
+        surf_path = os.path.join(temp_root, 'surface.png')
+        make_surface(surf_path)
+        curve_path = os.path.join(temp_root, 'curve.png')
+        make_curve(curve_path)
+
+        surface = np.array(Image.open(surf_path))
+        curve = np.array(Image.open(curve_path))
+
+    mask = (curve[..., 1] < 255)
+    surface[mask, :] = curve[mask, :]
+    Image.fromarray(surface[..., :3]).save(path)
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -19,7 +114,8 @@ extensions = ['sphinx.ext.autodoc']
 templates_path = ['_templates']
 exclude_patterns = []
 
-
+logo_path = os.path.join(__file__, '../_static/logo.png')
+make_logo(logo_path)
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
